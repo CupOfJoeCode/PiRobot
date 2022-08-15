b5903d2cd5064aad0b4baed107773940eb19c282
Joe Janicki <joejanicki26@gmail.com>
2022-08-15 18:40:39 -0400

    Add deploy script

---
+++ deploy.py
@@ -0,0 +1,15 @@
+import os
+import sys
+IP = '192.168.222.38'
+USERNAME = 'pi'
+PORT = 1234
+
+if '-r' in sys.argv:
+    os.system(
+        f'ssh -t {USERNAME}@{IP} "cd Robot ; python3 main.py {IP} {PORT}"')
+else:
+    print('Deploy')
+    os.system(f'ssh -t {USERNAME}@{IP} "rm -r Robot"')
+    print('Removed Old')
+    os.system(f'scp -r Robot {USERNAME}@{IP}:/home/{USERNAME}')
+    print('Deployed New')
