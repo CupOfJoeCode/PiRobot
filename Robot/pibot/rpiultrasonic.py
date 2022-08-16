bb0fcf69f2c628b1a62e9a75d2156e60f1d900a1
CupOfJoeCode <therealjoe2794@gmail.com>
2022-08-16 09:26:53 -0400

    Add ultrasonic sensor implementation

--- Robot/pibot/rpiultrasonic.py
+++ Robot/pibot/rpiultrasonic.py
@@ -1,10 +1,21 @@
+from pibot.rpioutput import RpiOutput
+from pibot.rpiinput import RpiInput
 from pibot.base.distance import DistanceSensor
+import time
 
 
 class RpiUltraSonic(DistanceSensor):
 
-    def __init__(self):
+    def __init__(self, trig_pin, echo_pin):
         super().__init__()
+        self.trigger = RpiOutput(trig_pin)
+        self.echo = RpiInput(echo_pin)
 
-    def get_seconds(self):
-        return super().get_seconds()
+    def get_seconds(self, timeout=1.0):
+        start_time = time.time()
+        self.trigger.pulse()
+        while not self.echo.get():
+            delta_time = time.time() - start_time
+            if delta_time > timeout:
+                break
+        return delta_time
