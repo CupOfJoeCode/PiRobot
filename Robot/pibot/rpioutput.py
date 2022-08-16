fb9e89e880d437c254e42b55d2303b68b1d1df0c
Joe Janicki <joejanicki26@gmail.com>
2022-08-15 21:21:14 -0400

    Gpio Output

--- Robot/pibot/rpioutput.py
+++ Robot/pibot/rpioutput.py
@@ -1,9 +1,13 @@
 from pibot.base.digitalout import DigitalOutput
+import RPi.GPIO as GPIO
 
 
 class RpiOutput(DigitalOutput):
-    def __init__(self):
+    def __init__(self, pin):
         super().__init__()
+        self.pin = pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
 
     def write(self, state):
-        return super().write(state)
+        GPIO.output(self.pin, state)
