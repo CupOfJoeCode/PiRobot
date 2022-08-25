80a4fd4278508c986ddf532aff203a43bd811ddc
Joe Janicki <joejanicki26@gmail.com>
2022-08-25 12:07:02 -0400

    Motor testing

--- Robot/pibot/rpihbridge.py
+++ Robot/pibot/rpihbridge.py
@@ -1,23 +1,31 @@
 from pibot.base.motor import Motor
 from pibot.rpipwm import RpiPWMOutput
-from pibot.rpioutput import RpiOutput
 
 
 class RpiHBridge(Motor):
 
-    def __init__(self, speed_pin, dir_pin, inverted=False, pwm_freq=100):
+    def __init__(self, pwm0, pwm1, inverted=False, pwm_freq=100):
         super().__init__()
-        self.pwm = RpiPWMOutput(speed_pin, pwm_freq)
-        self.direction = RpiOutput(dir_pin)
+        self.pwm0 = RpiPWMOutput(pwm0, pwm_freq)
+        self.pwm1 = RpiPWMOutput(pwm1, pwm_freq)
         self.speed = 0
-        self.pwm.set(0)
-        self.direction.set(False)
+        self.pwm0.set(0)
+        self.pwm1.set(0)
         self.inverted = inverted
 
     def set(self, speed):
-        self.pwm.set(abs(speed))
-        self.direction.set((speed < 0) != self.inverted)
+        if (speed < 0) != self.inverted:
+            self.pwm1.set(abs(speed))
+            self.pwm0.set(0)
+        else:
+            self.pwm0.set(abs(speed))
+            self.pwm1.set(0)
         self.speed = speed
 
+    def stop(self):
+        self.pwm0.set(0)
+        self.pwm1.set(0)
+        self.speed = 0
+
     def get(self):
-        return super().get()
+        return self.speed
--- Robot/robot.py
+++ Robot/robot.py
@@ -1,23 +1,32 @@
+from pibot.rpihbridge import RpiHBridge
+from pibot.rpioutput import RpiOutput
+
+MOTOR_FREQ = 5000
 
 
 class Robot:
     def __init__(self):
         self.data = {
-            'Num': 0
+            'Speed': 0.0,
+            'On': False
         }
         self.running = False
+        self.motor = RpiHBridge(12, 19, MOTOR_FREQ)
 
     def run(self):
-        pass
+        self.data['Speed'] = min(1, max(-1, self.data['Speed']))
+        self.motor.set(self.data['Speed'])
+        # self.output.write(self.data['On'])
 
     def stop(self):
-        pass
+        self.motor.stop()
+        self.data['Speed'] = 0.0
 
     def trigger_start(self, trigger):
         if trigger == 'up':
-            self.data['Num'] += 1
+            self.data['Speed'] += 0.05
         elif trigger == 'down':
-            self.data['Num'] -= 1
+            self.data['Speed'] -= 0.05
 
     def trigger_end(self, trigger):
         pass
