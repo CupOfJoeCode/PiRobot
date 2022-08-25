04174745502f3317981da2284e2a0a5ded7ef974
Joe Janicki <joejanicki26@gmail.com>
2022-08-25 12:08:11 -0400

    Change naming for pin args

--- Robot/pibot/rpihbridge.py
+++ Robot/pibot/rpihbridge.py
@@ -4,10 +4,10 @@ from pibot.rpipwm import RpiPWMOutput
 
 class RpiHBridge(Motor):
 
-    def __init__(self, pwm0, pwm1, inverted=False, pwm_freq=100):
+    def __init__(self, pin0, pin1, inverted=False, pwm_freq=100):
         super().__init__()
-        self.pwm0 = RpiPWMOutput(pwm0, pwm_freq)
-        self.pwm1 = RpiPWMOutput(pwm1, pwm_freq)
+        self.pwm0 = RpiPWMOutput(pin0, pwm_freq)
+        self.pwm1 = RpiPWMOutput(pin1, pwm_freq)
         self.speed = 0
         self.pwm0.set(0)
         self.pwm1.set(0)
