19baf66af7e8dc94c510b362b5201ae646842468
Joe Janicki <joejanicki26@gmail.com>
2022-08-25 22:23:19 -0400

    Testing dual motor setup

--- Robot/robot.py
+++ Robot/robot.py
@@ -1,5 +1,4 @@
 from pibot.rpihbridge import RpiHBridge
-from pibot.rpioutput import RpiOutput
 
 MOTOR_FREQ = 5000
 
@@ -7,26 +6,24 @@ MOTOR_FREQ = 5000
 class Robot:
     def __init__(self):
         self.data = {
-            'Speed': 0.0,
+            'SpeedLeft': 0.0,
+            'SpeedRight': 0.0,
             'On': False
         }
         self.running = False
-        self.motor = RpiHBridge(12, 19, pwm_freq=MOTOR_FREQ)
+        self.leftMotor = RpiHBridge(26, 19, pwm_freq=MOTOR_FREQ)
+        self.rightMotor = RpiHBridge(13, 12, pwm_freq=MOTOR_FREQ)
 
     def run(self):
-        self.data['Speed'] = min(1, max(-1, self.data['Speed']))
-        self.motor.set(self.data['Speed'])
-        # self.output.write(self.data['On'])
+        self.leftMotor.set(self.data['SpeedLeft'])
+        self.rightMotor.set(self.data['SpeedRight'])
 
     def stop(self):
-        self.motor.stop()
-        self.data['Speed'] = 0.0
+        self.leftMotor.stop()
+        self.rightMotor.stop()
 
     def trigger_start(self, trigger):
-        if trigger == 'up':
-            self.data['Speed'] += 0.05
-        elif trigger == 'down':
-            self.data['Speed'] -= 0.05
+        pass
 
     def trigger_end(self, trigger):
         pass
