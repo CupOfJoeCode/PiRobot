4f67eb3f2b110ad80677dd61be261a5913e3c74e
Joe Janicki <joejanicki26@gmail.com>
2022-12-08 11:43:44 -0500

    Add commands and base robot

---
+++ Robot/pibot/base/baserobot.py
@@ -0,0 +1,34 @@
+class BaseRobot:
+    def __init__(self):
+        self.data = {
+        }
+        self.running = False
+
+        self.triggers = {}
+        self.binds = {}
+
+    def run(self):
+        for bind in self.binds:
+            if self.triggered(bind):
+                self.binds[bind].run()
+                
+            elif self.binds[bind].started:
+                self.binds[bind].stop()
+                self.binds[bind].reset()
+
+    def stop(self):
+        pass
+
+    def bind(self,trigger,command):
+        self.binds[trigger] = command
+
+    def triggered(self,trigger):
+        if trigger not in self.triggers:
+            return False
+        return self.triggers[trigger]
+
+    def trigger_start(self, trigger):
+        self.triggers[trigger] = True
+
+    def trigger_end(self, trigger):
+        self.triggers[trigger] = False
---
+++ Robot/pibot/base/command.py
@@ -0,0 +1,53 @@
+class Command:
+    def __init__(self,name):
+        self.name = name
+        self.initialize_handler = lambda: None
+        self.execute_handler = lambda: None
+        self.until_handler = lambda: False
+        self.end_handler = lambda: None
+        self.running = True
+        self.started = False
+    def run(self):
+        if not self.running:
+            return
+        if not self.started:
+            self.started = True
+            return self.initialize_handler()
+        self.execute_handler()
+        if self.until_handler():
+            self.end_handler()
+            self.running = False
+    
+
+    def reset(self):
+        self.running = True
+        self.started = False
+
+    def stop(self):
+        self.running = False
+        self.end_handler()
+
+    def initialize(self,handler):
+        self.initialize_handler = handler
+        return self
+    
+    def execute(self,handler):
+        self.execute_handler = handler
+        return self
+    
+    def until(self,handler):
+        self.until_handler = handler
+        return self
+
+    def end(self,handler):
+        self.end_handler = handler
+        return self
+    
+    def with_name(self,name):
+        self.name = name
+        return self
+    
+    def __repr__(self):
+        return f'Command({self.name})'
+
+        
