214433a112df8f7c138a47a1ef3820d6df9a8736
CupOfJoeCode <therealjoe2794@gmail.com>
2023-04-28 21:13:46 -0400

    Document digital output and input

--- Robot/pibot/base/digitalin.py
+++ Robot/pibot/base/digitalin.py
@@ -1,6 +1,30 @@
 class DigitalInput:
+    """
+    An input that returns a boolean value
+
+    ...
+
+    Methods
+    -------
+    get()
+        Get the current input state
+    """
+
     def __init__(self) -> None:
+        """Create a digital input
+
+        Returns
+        -------
+        None
+        """
         pass
 
     def get(self) -> bool:
+        """Get the state of the input
+
+        Returns
+        -------
+        state : bool
+            The state of the input
+        """
         return False
--- Robot/pibot/base/digitalout.py
+++ Robot/pibot/base/digitalout.py
@@ -1,20 +1,85 @@
 from time import sleep
+from pibot.base.units import Time
 
 
 class DigitalOutput:
+    """
+    An output that writes a boolean value
+
+    ...
+
+    Attributes
+    ----------
+    state : bool
+        the current state of the output
+
+    Methods
+    -------
+    write(state : bool)
+        Write a value to the output
+    set()
+        Turn the output on
+    reset()
+        Turn the output off
+    pulse(time : Time, low : bool)
+        Pulse the output for a specified amount of time
+    """
+
     def __init__(self) -> None:
+        """Creates a digital output
+
+        Returns
+        -------
+        None
+        """
         self.state = False
 
     def write(self, state: bool) -> None:
+        """Write a value to the output
+
+        Parameters
+        ----------
+        state : bool
+            The value to write to the output
+
+        Returns
+        -------
+        None
+        """
         self.state = state
 
     def set(self) -> None:
+        """Turn the output on
+
+        Returns
+        -------
+        None
+        """
         self.write(True)
 
     def reset(self) -> None:
+        """Turn the output off
+
+        Returns
+        -------
+        None
+        """
         self.write(False)
 
-    def pulse(self, time: float = 0.01, low: bool = False) -> None:
+    def pulse(self, time: Time = Time.from_milliseconds(10), low: bool = False) -> None:
+        """Pulse the output for a specified amount of time
+
+        Parameters
+        ----------
+        time : Time, optional
+            How long the pulse should last
+        low : bool, optional
+            Whether it should pulse low instead of high
+
+        Returns
+        -------
+        None
+        """
         self.write(True != low)
-        sleep(time)
+        sleep(time.get_seconds())
         self.write(False != low)
