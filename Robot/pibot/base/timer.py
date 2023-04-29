147b7e18bdb5826fcccda8768686bc065f4c78e4
CupOfJoeCode <therealjoe2794@gmail.com>
2023-04-29 11:28:53 -0400

    Document timer

--- Robot/pibot/base/timer.py
+++ Robot/pibot/base/timer.py
@@ -3,11 +3,41 @@ from pibot.base.units import Time
 
 
 class Timer:
+    """
+    A way to keep track of elapsed time
+
+    Methods
+    -------
+    start()
+        Start the timer
+    get_time()
+        Get the elapsed time since the timer was started
+    """
+
     def __init__(self) -> None:
+        """Create a timer
+
+        Returns
+        -------
+        None
+        """
         self._initial_time = time.time()
 
     def start(self) -> None:
+        """Start the timer
+
+        Returns
+        -------
+        None
+        """
         self._initial_time = time.time()
 
     def get_time(self) -> Time:
+        """Get the elapsed time since the timer was started
+
+        Returns
+        -------
+        time : Time
+            The elapsed time since the timer was started
+        """
         return Time.from_seconds(time.time() - self._initial_time)
