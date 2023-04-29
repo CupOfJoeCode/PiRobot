fa960bd565da2f8da6e7382549f16238f5523aa7
CupOfJoeCode <therealjoe2794@gmail.com>
2023-04-29 10:58:11 -0400

    Document PWM output

--- Robot/pibot/base/pwmout.py
+++ Robot/pibot/base/pwmout.py
@@ -1,6 +1,37 @@
 class PWMOutput:
+    """
+    An oscillating output whose pulse with can be changed
+
+    Attributes
+    ----------
+    width : float
+        The pulse width in the range [0.0,1.0]
+
+    Methods
+    -------
+    set(width : float)
+        Set the pulse width
+    """
+
     def __init__(self) -> None:
+        """Create a new PWM output
+
+        Returns
+        -------
+        None
+        """
         self.width = 0
 
     def set(self, width: float) -> None:
+        """Set the pulse width
+
+        Parameters
+        ----------
+        width : float
+            The pulse width in the range [0.0,1.0]
+
+        Returns
+        -------
+        None
+        """
         self.width = min(1.0, max(0.0, width))
