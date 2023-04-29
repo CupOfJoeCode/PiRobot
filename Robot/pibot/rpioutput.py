8ad622d00b386e41d26a29c2d54e43a2511b2459
CupOfJoeCode <therealjoe2794@gmail.com>
2023-04-29 11:56:02 -0400

    Document rpi output

--- Robot/pibot/rpioutput.py
+++ Robot/pibot/rpioutput.py
@@ -3,11 +3,43 @@ import RPi.GPIO as GPIO
 
 
 class RpiOutput(DigitalOutput):
+    """
+    A digital output connected to a Raspberry Pi.
+    Inherits from `DigitalOutput`
+
+    Methods
+    -------
+    write(state : bool)
+        Write a state to the output
+    """
+
     def __init__(self, pin: int) -> None:
+        """Create a Raspberry Pi digital output
+
+        Parameters
+        ----------
+        pin : int
+            The output pin of the Raspberry Pi using the BCM number
+
+        Returns
+        -------
+        None
+        """
         super().__init__()
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.OUT)
 
     def write(self, state: bool) -> None:
+        """Write a state to the output
+
+        Parameters
+        ----------
+        state : bool
+            The state of the output
+
+        Returns
+        -------
+        None
+        """
         GPIO.output(self.pin, state)
