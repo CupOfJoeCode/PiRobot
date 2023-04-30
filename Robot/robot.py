27466583231c9dfb63ea4aaac680effe69e875a0
CupOfJoeCode <therealjoe2794@gmail.com>
2023-04-29 22:08:11 -0400

    Get rid of drive stuff for now

--- Robot/robot.py
+++ Robot/robot.py
@@ -1,11 +1,6 @@
 # from robotmap import RobotMap
 from basemap import BaseMap
 from pibot.base.baserobot import BaseRobot
-from pibot.base.pose import Pose2d, Pose3d
-from pibot.base.units import Distance, Angle
-from pibot.base.rotation import Rotation2d
-
-DRIVE_SPEED = 1.0
 
 
 class Robot(BaseRobot):
@@ -13,32 +8,8 @@ class Robot(BaseRobot):
         super().__init__()
         robot_map = BaseMap()
 
-        pose_test = Pose2d(
-            Distance.from_meters(1),
-            Distance.from_meters(-0.5),
-            Rotation2d(Angle.from_degrees(45)),
-        )
-
-        self.data.put_pose2d("pose", pose_test)
-
-        self.left_motor = robot_map.get_left_motor()
-        self.right_motor = robot_map.get_right_motor()
-
-    def drive(self, left: float, right: float) -> None:
-        self.left_motor.set(left)
-        self.right_motor.set(right)
-
     def run(self) -> None:
         super().run()
-        if self.triggered("up"):
-            self.drive(1.0, 1.0)
-        elif self.triggered("down"):
-            self.drive(-1.0, -1.0)
-
-        if self.triggered("left"):
-            self.drive(-1.0, 1.0)
 
     def stop(self) -> None:
         super().stop()
-        self.left_motor.stop()
-        self.right_motor.stop()
--- Robot/robotmap.py
+++ Robot/robotmap.py
@@ -1,16 +1,6 @@
 from basemap import BaseMap
-from pibot.base.motor import Motor
-from pibot.rpihbridge import RpiHBridge
-
-MOTOR_FREQ = 5000
 
 
 class RobotMap(BaseMap):
     def __init__(self):
         pass
-
-    def get_left_motor(self) -> Motor:
-        return RpiHBridge(26, 19, pwm_freq=MOTOR_FREQ)
-
-    def get_right_motor(self) -> Motor:
-        return RpiHBridge(13, 12, pwm_freq=MOTOR_FREQ)
